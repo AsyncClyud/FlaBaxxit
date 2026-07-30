439ccd75a45b78687fe214b6e1740c0de7f9b3d8
AsyncClyud <AsyncClyud@outlook.com>
2026-07-30 17:59:44 +0300

    Make structure more cleaner; Add profile service function

--- internal/service/post/article-service.go
+++ internal/service/post/article-service.go
@@ -68,28 +68,3 @@ func (pr PostService) UpdateArticle(ctx context.Context, article models.Article)
 func (pr PostService) DeleteArticle(ctx context.Context, article models.Article) {
 	pr.repo.DeleteArticle(ctx, article)
 }
-
-func (pr PostService) ValidateComment(comment models.Comment) (status_code int) {
-	if len(comment.Comment_content) == 0 {
-		return http.StatusBadRequest
-	}
-	return http.StatusOK
-}
-
-func (pr PostService) GetArticleCommentsById(ctx context.Context, id int) (comments string) {
-	return pr.repo.GetArticleCommentsById(ctx, id)
-}
-
-func (pr PostService) InsertComment(ctx context.Context, comment models.Comment, author_id int) (status_code int) {
-	status := pr.ValidateComment(comment)
-	if status != http.StatusOK {
-		return status
-	}
-	contains_bad_content := moderation.ModerateText(comment.Comment_content)
-	if contains_bad_content == true {
-		return http.StatusUnprocessableEntity
-	}
-
-	pr.repo.InsertComment(ctx, comment, author_id)
-	return http.StatusOK
-}
---
+++ internal/service/user/auth-service.go
@@ -0,0 +1,113 @@
+package userservice
+
+import (
+	"blog/internal/models"
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (ur *AuthService) HashPassword(password string) (hashed_password string, err error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func (ur *AuthService) CheckPaswordHash(password string, hashedPassword string) (err error) {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (ur *AuthService) Generate_Token(id int) (token string, err error) {
+	claims := jwt.MapClaims{
+		"UserID": id,
+		"exp":    time.Now().Add(24 * time.Hour).Unix(),
+	}
+	new_token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+
+	return new_token.SignedString(ur.secret_key)
+
+}
+
+func (ur *AuthService) Validate_Token(tokenString string) (Id int, err error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		return ur.secret_key, nil
+	})
+	if err != nil {
+		return 0, fmt.Errorf("Parsing error: %v", err)
+	}
+	if !token.Valid {
+		return 0, fmt.Errorf("Invalid token")
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, fmt.Errorf("Invalid claims")
+	}
+	userIDValue, exists := claims["UserID"]
+	if !exists {
+		return 0, fmt.Errorf("Invalid UserID")
+	}
+	userID := userIDValue.(float64)
+
+	return int(userID), nil
+}
+
+func (ur *AuthService) SetTokenInCookie(c *gin.Context, id int) {
+	jwt_token, err := ur.Generate_Token(id)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	c.SetCookie("jwt-token", jwt_token, 3200*20, "/", "", true, true)
+}
+
+func (ur *AuthService) ValidateUserData(user models.User) (status_code int) {
+	if strings.ContainsAny(user.Username, ` !@#$%^&*();:{}[]"'.,?/-_+=`) == true {
+		return http.StatusNotAcceptable
+	}
+	if len(user.Username) <= 2 {
+		return http.StatusBadRequest
+	}
+	if len(user.Password) <= 5 {
+		return http.StatusUnprocessableEntity
+	}
+	return http.StatusOK
+}
+
+func (ur *AuthService) Register(ctx context.Context, user models.User) (status_code, id int) {
+	if code := ur.ValidateUserData(user); code != http.StatusOK {
+		return code, 0
+	}
+	hashed_password, err := ur.HashPassword(user.Password)
+	if err != nil {
+		return http.StatusBadGateway, 0
+	}
+	new_user := models.User{Username: user.Username, Password: hashed_password}
+
+	id, message := ur.repo.CreateUser(ctx, new_user)
+	if !message {
+		return http.StatusConflict, 0
+	}
+	return http.StatusOK, id
+}
+
+func (ur *AuthService) Login(ctx context.Context, user models.User) (status_code, id int) {
+	if code := ur.ValidateUserData(user); code != http.StatusOK {
+		return code, 0
+	}
+	id, hashed_password, ok := ur.repo.CheckIfUserExist(ctx, user)
+	if !ok {
+		return http.StatusNotFound, 0
+	}
+	if ur.CheckPaswordHash(user.Password, hashed_password) != nil {
+		return http.StatusNotFound, 0
+	}
+	return http.StatusOK, id
+}
